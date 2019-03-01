a402d4d272ecf5778bd9d6a23594aa4dff4229cf
Student <example@example.com>
2019-03-01 08:49:17 -0800

    add better function to nav-bar on nav.html

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import './sass/styles.scss';
 import $ from 'jquery';
 
 $(document).ready(function() {
-  $(".burger").click(function() {
+  $(".nav").click(function() {
     $("#dropdown").fadeToggle();
   });
 });
